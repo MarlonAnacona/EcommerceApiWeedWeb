2a2a5518fecc9d3590c2b6963967f906306a31cf
MarlonAnacona <marlon.andres.anacona@gmail.com>
2023-12-18 17:28:24 -0500

    fix error cors

--- src/main/java/com/API/Pizzapp/Config/SecurityConfig.java
+++ src/main/java/com/API/Pizzapp/Config/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -40,8 +46,23 @@ public class SecurityConfig {
                         sessionManager
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource()))
                 .build();
 
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // Permite cualquier origen
+        configuration.setAllowedMethods(Collections.singletonList("*")); // Permite todos los métodos
+        configuration.setAllowedHeaders(Collections.singletonList("*")); // Permite todas las cabeceras
+        configuration.setAllowCredentials(true); // Permite credenciales
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Link")); // Expone ciertas cabeceras
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplica la configuración a todas las rutas
+        return source;
     }
 }
 
